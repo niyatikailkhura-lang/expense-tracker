c29730aedf507abc5d86a757e6b55e2be2174958
Niyati Kailkhura <niyatikailkhura@gmail.com>
2026-06-29 22:46:50 +0530

    save and delete from local storgae

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 localStorage.setItem("test", "Hello");
 let expenselist=JSON.parse(localStorage.getItem("expenses"))||[];
+let expenses=document.getElementById("expenses");
 console.log(expenselist);
+for(let i=0;i<expenselist.length;i++){
+    let expense=expenselist[i];
+    let card=document.createElement("div");
+    card.className="card";
+    card.innerHTML=`
+    <h3>${expense.category}</h3>
+     <p>💰 Amount: ₹${expense.amount}</p>
+    <p>📅 Date:${expense.date}</p>
+    <button class="delete-button">delete🗑️</button>`
+     card.querySelector(".delete-button").addEventListener("click",function(){
+        expenselist.splice(i, 1);
+        localStorage.setItem("expenses", JSON.stringify(expenselist));
+        card.remove();
+    });
+    expenses.appendChild(card);
+}
 let button=document.getElementById("mybutton");
 button.addEventListener("click",function(){
     let amount=document.getElementById("amount").value
